4299c3be9e900288ab96b66d9bcb7bf8d315cf3d
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-07-06 17:28:22 -0500

    Updates

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, abort
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
@@ -16,6 +16,26 @@ CORS(app)
 # Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+@app.errorhandler(404)
+def not_found_error(error):
+    response = jsonify({
+        "404": 404,
+        "error": "Not Found",
+        "message": error.description if
+        error.description else "The requested URL was not found on the server."
+    })
+    response.status_code = 404
+    return response
+
+@app.errorhandler(500)
+def not_found_error(error):
+    response = jsonify({
+        "500": 500,
+        "error": "Internal Server Error",
+        "message": error.description if error.description else "The server return a error."
+    })
+    response.status_code = 404
+    return response
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -37,7 +57,40 @@ def handle_hello():
                      "family": members}
     return jsonify(response_body), 200
 
+@app.route('/members', methods=['POST'])
+def handle_crear():
+    # This is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+    response_body = {"hello": "creado con exito",
+                     "family": members}
+    return jsonify(response_body), 201
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def handle_leer(member_id):
+    try:
+        members = jackson_family.get_member(member_id)
+        response_body = members
+        return jsonify(response_body), 200
+    except:
+        abort(404)
+
+@app.route('/members/<int:member_id>', methods=['POST'])
+def handle_actualizar(member_id):
+    try:
+        members = jackson_family.get_member(member_id)
+        response_body = members
+        return jsonify(response_body), 204
+    except:
+        abort(404)
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def handle_eliminar(member_id):
+    try:
+        members = jackson_family.get_member(member_id)
+        response_body = members
+        return jsonify(response_body), 204
+    except:
+        abort(404)
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -16,7 +16,21 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
-            }
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            },
         ]
 
     # This method generates a unique incremental ID
@@ -36,9 +50,8 @@ class FamilyStructure:
         pass
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        member = self._members[id - 1]
+        return member
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
